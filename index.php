978404447c4a6e65dd34288023e96a20a89857b8
陳力睿 <jerrylj.chen@gmail.com>
2016-08-18 11:36:22 +0800

    revised $data json_encode

--- index.php
+++ index.php
@@ -5,10 +5,12 @@ header('content-type: application/json; charset=utf-8');
 // header('access-control-allow-origin:*');
 
 $data = ['money'=> 20000000, 'name'=>'傑瑞', 'Job'=>'Chinese Medical Doctor'];
+
+$data = json_encode($data);
+
 if (isset($_GET['callback'])) {
-    $data = json_encode($data);
     echo "{$_GET['callback']}($data)";
 } else {
-    echo json_encode($data);
+    echo $data;
 }
 
