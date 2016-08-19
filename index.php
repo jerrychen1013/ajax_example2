56c928fdc1a59b94f3d9f8ec443a21d37152a5cb
KKJ <jerrylj.chen@gmail.com>
2016-08-19 18:12:21 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php 
 //執行 php -S localhost:8002  啟動php內建server在localhost:8002 port
 
+//CROS作法
+//header('Access-Control-Allow-Origin: http://base.com');
+//header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+//header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
+
 // 允許所有其他Domain進入
 header('content-type: application/json; charset=utf-8');
 // header('access-control-allow-origin:*');
