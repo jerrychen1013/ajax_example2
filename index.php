b4357392ee135c93f73ea22c42eb5a1f91820aac
JerryChen <jerrylj.chen@gmail.com>
2016-08-24 17:51:30 +0800

    wording fixed

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 //執行 php -S localhost:8002  啟動php內建server在localhost:8002 port
 
 //CROS作法
-//header('Access-Control-Allow-Origin: http://base.com');
-//header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
-//header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
+// header('Access-Control-Allow-Origin: http://base.com');
+// header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+// header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Accept');
 
 // 允許所有其他Domain進入, CROS作法
 // header('access-control-allow-origin: *');
